1b21830e92f02da1efc1ecf43e997e3ec7e781af
Haneet Singh Chhabra <haneet555@gmail.com>
2022-03-24 00:52:36 +0530

    some small changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ admin.initializeApp(mConfig.firebaseConfig);
 
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
-exports.addusers = functions.https.onRequest(async (req, res) => {
+exports.addNpUsers = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
    // const original = req.query.text;
 
@@ -45,10 +45,11 @@ exports.addusers = functions.https.onRequest(async (req, res) => {
     userDOB: req.query.userDOB,
     userID: req.query.userID,
     userLocation:req.query.userLocation,
-    userName:req.query.userName
+    userName:req.query.userName,
+    userType:"NP"
   };
   // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult =  await admin.firestore().collection('users').add(userModel).catch((error) => {
+  const writeResult =  await admin.firestore().collection('natualPerson').doc("NP-"+req.query.userID).set(userModel).catch((error) => {
     var response={
       "status_message": "Fail",
       "status_code":5,
@@ -79,7 +80,7 @@ exports.addusers = functions.https.onRequest(async (req, res) => {
     "data": {
     // userDetail:userDetail
     },
-    "message": "User with ID: "+writeResult.id+" added." /* Or optional success message */
+    "message": "User with Name: "+req.query.userName+" added Successfully." /* Or optional success message */
   }
   return res.json({result: response});
 
